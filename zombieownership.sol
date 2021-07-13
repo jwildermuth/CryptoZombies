789d21669045ea6233337ad880b283ceb9433b1b
Jason Wildermuth <jmw33322@hotmail.com>
2021-07-13 14:27:40 -0400

    Add comments

--- zombieownership.sol
+++ zombieownership.sol
@@ -3,18 +3,31 @@ pragma solidity >=0.5.0 <0.6.0;
 import "./zombieattack.sol";
 import "./erc721.sol";
 
+/// @title A contract for Zombie ownership functions
+/// @author Jason Wildermuth
+/// @dev Zombie ownership functions that are ERC721 compliant
+
 contract ZombieOwnership is ZombieAttack, ERC721 {
 
     mapping (uint => address) zombieApprovals;
 
+    /**
+    * @return number of zombies _owner has
+    */
     function balanceOf(address _owner) external view returns (uint256) {
         return ownerZombieCount[_owner];
     }
 
+    /**
+    * @return address for owner of _tokenId
+    */
     function ownerOf(uint256 _tokenId) external view returns (address) {
         return zombieToOwner[_tokenId];
     }
 
+    /**
+    * @dev Perform our app specific chores for a token transfer
+    */
     function _transfer(address _from, address _to, uint256 _tokenId) private {
         ownerZombieCount[_to] = ownerZombieCount[_to].add(1);
         ownerZombieCount[_from] = ownerZombieCount[_from].sub(1);
@@ -22,11 +35,17 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
         emit Transfer(_from, _to, _tokenId);
     }
 
+    /**
+    * @dev Sender must be owner of token or have been approved
+    */
     function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
         require(zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
         _transfer(_from, _to, _tokenId);
     }
 
+    /**
+    * @dev Add an approved address for using the transferFrom function
+    */
     function approve(address _approved, uint256 _tokenId) external payable onlyOwnerOf(_tokenId) {
         zombieApprovals[_tokenId] = _approved;
         emit Approval(msg.sender, _approved, _tokenId);
