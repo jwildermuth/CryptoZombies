61d0a87746018f949be208e614d16d72a3376c0b
Jason Wildermuth <jmw33322@hotmail.com>
2021-07-14 12:29:55 -0400

    Recompile contracts and adjust gas limit for Rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -60,8 +60,8 @@ module.exports = {
     rinkeby: {
       provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/740afd10d606463db4376f1e77fb1d8d`),
       network_id: 4,       // rinkeby's id
-      gas: ,        // rinkeby has a lower block limit than mainnet
-      confirmations: 1,    // # of confs to wait between deployments. (default: 0)
+      gas: 30000000,        // rinkeby has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
